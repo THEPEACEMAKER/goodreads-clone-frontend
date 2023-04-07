9d9d42a855e6166b7aed66fd6df09eeb77131f60
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2023-04-07 23:48:00 +0200

    render book description

--- src/app/interfaces/book.interface.ts
+++ src/app/interfaces/book.interface.ts
@@ -1,6 +1,7 @@
 export interface Book {
   _id?: number;
   name: string;
+  description: string;
   imageUrl?: string;
   category: number[] | []; // reference to Category
   author: number[] | []; // reference to Author
