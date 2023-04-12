8d976a4758dcf199100fffdf0916f41c236562e3
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2023-04-12 11:13:03 +0200

    redirect to home on admin logout

--- src/app/admin-header/admin-header.component.ts
+++ src/app/admin-header/admin-header.component.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-header',
   templateUrl: './admin-header.component.html',
-  styleUrls: ['./admin-header.component.css']
+  styleUrls: ['./admin-header.component.css'],
 })
 export class AdminHeaderComponent {
-  constructor(private _authService:AuthService){
-
-  }
+  constructor(private _authService: AuthService, private router: Router) {}
 
   logout() {
     this._authService.logoutUser();
+    this.router.navigate(['/']);
   }
-  
 
   isToggled = false;
 
