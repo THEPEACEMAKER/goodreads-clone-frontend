cab1ac674aa8a4d6873a8195540a1e323c3ebd51
mohamedosama298 <mohamedosama135246@gmail.com>
2023-04-13 01:44:24 +0200

    styling pagination & setting temp for home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BookService } from '../services/book.service';
+import { Author, Book } from '../interfaces';
+import { AuthorService } from '../services/author.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  books:Book[]=[];
+  authors:Author[]=[];
+  constructor(private _bookService: BookService,private _authorService: AuthorService){
+    _bookService.getBooks().subscribe({
+      next: (response:any) =>{
+        this.books=response.books;
+      }
+    })
+
+    _authorService.getAuthors().subscribe({
+      next: (response:any) =>{
+        this.authors=response.authors;
+      }
+    });
+  }
+
 
 }
