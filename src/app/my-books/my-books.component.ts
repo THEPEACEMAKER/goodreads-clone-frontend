8a110123246833d8344c67eeca6bba3af4ac3e00
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2023-04-15 22:40:19 +0200

    add pagination to mybooks

--- src/app/my-books/my-books.component.ts
+++ src/app/my-books/my-books.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { BookService } from '../services/book.service';
-import { Book} from '../interfaces';
+import { Book } from '../interfaces';
 import { ShelfService } from '../services/shelf.service';
-
-
 
 @Component({
   selector: 'app-my-books',
   templateUrl: './my-books.component.html',
-  styleUrls: ['./my-books.component.css']
+  styleUrls: ['./my-books.component.css'],
 })
 export class MyBooksComponent {
   allBooks: Book[] = [];
@@ -16,12 +14,16 @@ export class MyBooksComponent {
   wantToReadBooks: Book[] = [];
   currentlyReadingBooks: Book[] = [];
   selectedContent = 'all';
+  page = 1; // current page
+  perPage = 10; // books per page
+  totalBooks = 0;
 
-  constructor(private bookService: BookService, private ShelfService:ShelfService) {}
+  constructor(private bookService: BookService, private ShelfService: ShelfService) {}
 
   ngOnInit(): void {
     this.ShelfService.getUserBooks().subscribe((response) => {
       this.allBooks = response.books;
+      this.totalBooks = response.totalBooks;
     });
   }
 
@@ -30,35 +32,33 @@ export class MyBooksComponent {
 
     switch (content) {
       case 'all':
-        if (!this.allBooks.length) {
-          this.ShelfService.getUserBooks().subscribe((response) => {
-            this.allBooks = response.books;
-          });
-        }
+        this.ShelfService.getUserBooks('', this.page, this.perPage).subscribe((response) => {
+          this.allBooks = response.books;
+          this.totalBooks = response.totalBooks;
+        });
         break;
       case 'read':
-        if (!this.readBooks.length) {
-          this.ShelfService.getUserBooks('READ').subscribe((response) => {
-            this.readBooks = response.books;
-          });
-        }
+        this.ShelfService.getUserBooks('READ', this.page, this.perPage).subscribe((response) => {
+          this.readBooks = response.books;
+          this.totalBooks = response.totalBooks;
+        });
         break;
       case 'want-to-read':
-        if (!this.wantToReadBooks.length) {
-          this.ShelfService.getUserBooks('WANT_TO_READ').subscribe((response) => {
+        this.ShelfService.getUserBooks('WANT_TO_READ', this.page, this.perPage).subscribe(
+          (response) => {
             this.wantToReadBooks = response.books;
-          });
-        }
+            this.totalBooks = response.totalBooks;
+          }
+        );
         break;
       case 'currently-reading':
-        if (!this.currentlyReadingBooks.length) {
-          this.ShelfService.getUserBooks('CURRENTLY_READING').subscribe((response) => {
+        this.ShelfService.getUserBooks('CURRENTLY_READING', this.page, this.perPage).subscribe(
+          (response) => {
             this.currentlyReadingBooks = response.books;
-          });
-        }
+            this.totalBooks = response.totalBooks;
+          }
+        );
         break;
     }
   }
 }
-
-
